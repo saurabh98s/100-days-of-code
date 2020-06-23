56499e2718e4a2dce633da30b14b8aac5e79e9e2
saurabh98s <geek.saurabh98@gmail.com>
2020-06-23 06:40:59 +0530

    adding STL implementation of vector

--- day6_arrays/arrays.cpp
+++ day6_arrays/arrays.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
+#include<vector>
+
 using namespace std;
 
-
-int main() {
-    cout<<"Hello world";
+int main(){
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(50);
+    v.push_back(90);
+    cout<<"SIze is "<<v.size()<<endl;
+    v.clear();
+    cout<<"SIze is "<<v.size()<<endl;
     return 0;
 }
