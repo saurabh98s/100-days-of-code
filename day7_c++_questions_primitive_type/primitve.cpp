11a97acc0cd0b89147922632898f76f6ee99f6d4
saurabh98s <geek.saurabh98@gmail.com>
2020-06-28 07:47:31 +0530

    day 8

--- day7_c++_questions_primitive_type/primitve.cpp
+++ day7_c++_questions_primitive_type/primitve.cpp
@@ -1,18 +1,30 @@
-#include<stdio.h>
-#include<iostream>
+#include <stdio.h>
+#include <iostream>
 using namespace std;
 
+short parity1(unsigned long x)
+{
+    short result = 0;
+    while (x)
+    {
+        result ^= (x & 1); //^ means XOR & is AND Operation
+        x >>= 1;           //right shift bit operator
+    }
 
- short parity1(unsigned long x) {
-       short result = 0;
-       while (x) {
-          result ^= (x & 1);
-          x >>= 1;
-          }
-         
-           return result;
+    return result;
 }
 
-int main() {
-    cout<<parity1(50);
+short parity2(unsigned long x)
+{
+    short result = 0;
+    while (x) {
+        result^=1;
+        x&=(x-1);
+    }
+    return result;
+}
+
+int main()
+{
+    cout << parity1(50);
 }
