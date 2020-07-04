d78d1b62bf392936d2698457eb465db9b6df9cac
saurabh98s <geek.saurabh98@gmail.com>
2020-07-04 11:36:58 +0530

    array check

--- day9_coding_interview_questions/array.go
+++ day9_coding_interview_questions/array.go
@@ -2,7 +2,6 @@ package main
 
 import "fmt"
 
-
 //isItUnique uses maps to find the result
 func isItUnique(input string) bool {
 	seen := make(map[rune]bool)
@@ -18,22 +17,34 @@ func isItUnique(input string) bool {
 
 // isUnique uses no additional data structures
 func isUnique(input string) bool {
-	if len(input)>128 {
+	if len(input) > 128 {
 		return false
 	}
-	boolArray:=[128]bool{}
+	boolArray := [128]bool{}
 	for _, r := range input {
-		ok:=int(r)
+		ok := int(r)
 		if boolArray[ok] {
 			return false
-			
+
 		}
-		boolArray[ok]=true
+		boolArray[ok] = true
 	}
 	return true
 }
 
 func main() {
+	boolArray := [128]bool{}
+	/*
+	checking if an initialized bool array has all values true or false
+	Here it is all false
+	*/
+	for _, r := range boolArray {
+		if r!=true {
+			print(r)
+		}
+
+	}
+
 	fmt.Println(isItUnique("Hello"))
 	fmt.Println(isItUnique("Hello"))
 	fmt.Println(isItUnique("Helo"))
