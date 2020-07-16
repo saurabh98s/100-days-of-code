66b8dd844b91d65951bd8464352e55b1bae8bbb3
saurabh98s <geek.saurabh98@gmail.com>
2020-07-16 21:41:38 +0530

    add one missing q5

---
+++ day11_string_manipulation/one_missing.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func checkOneMissing(input1 string, input2 string) bool {
+	charMap := make(map[rune]int)
+	char := 0
+
+	for _, val := range input1 {
+		charMap[val]++
+		fmt.Println("inside input 1",charMap[val])
+	}
+	for _, val := range input2 {
+		charMap[val]--
+		fmt.Println("inside input 2",charMap[val])
+	}
+	for _, val := range charMap {
+		if val < 0 {
+			char++
+			
+		}
+	}
+	fmt.Println(char)
+	if char > 1 {
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(checkOneMissing("panic", "panixl"))
+
+}
