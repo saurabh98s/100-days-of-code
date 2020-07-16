bf68a8ddedd9fba4b761034e9b1e92f19b4927f4
saurabh98s <geek.saurabh98@gmail.com>
2020-07-16 13:24:58 +0530

    adding permutation Q4 string

---
+++ day11_string_manipulation/permutation.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func permutationPalindrome(str string) bool {
+	charMap := make(map[rune]int)
+	for _, char := range str {
+		if char != rune(' ') {
+			charMap[char]++
+		}
+	}
+	oddCount := 0
+	isPermutation := true
+
+	for _, value := range charMap {
+		if value%2 == 1 {
+			oddCount++
+		}
+		if oddCount > 1 {
+			isPermutation = false
+			break
+		}
+	}
+	return isPermutation
+
+}
+
+func main() {
+	fmt.Println(permutationPalindrome("Taaut"))
+
+}
