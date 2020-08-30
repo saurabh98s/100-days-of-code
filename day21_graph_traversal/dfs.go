0da51165d1095f545e0fbd70ba95500fb6a94d9b
saurabh98s <geek.saurabh98@gmail.com>
2020-08-30 07:58:34 +0530

    fix documentation for dfs.go

--- day21_graph_traversal/dfs.go
+++ day21_graph_traversal/dfs.go
@@ -11,8 +11,11 @@ type Node struct {
 
 // DFS traverse along the graph keeping a depth first approach
 func (node *Node) DFS(array []string) []string {
+	// append the node which we touched first
 	array=append(array, node.name)
 
+	// loop through the children recursively and visit each node
+	// and append them to the array
 	for _, childNodes := range node.children {
 		array= childNodes.DFS(array)
 	}
